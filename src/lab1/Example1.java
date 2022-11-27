08fc58487de4a15fc3d0bbce526716d3433d646a
DmitrySofronov <sofronov.d.v@yandex.ru>
2022-11-27 21:40:45 +0500

    добавил два восклицательных знака в текст, выводимый в консоль

--- src/lab1/Example1.java
+++ src/lab1/Example1.java
@@ -2,6 +2,7 @@ package lab1;
 
 public class Example1 {
     public static void main(String[] args) {
-        System.out.println("Привет мир!");
+        System.out.println("Привет мир!!!");
+
     }
 }
