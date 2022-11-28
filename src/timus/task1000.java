6b20b1055fb4d36378b62f37b031f3666627e9db
DmitrySofronov <sofronov.d.v@yandex.ru>
2022-11-28 17:43:45 +0500

    решил задачу 1000

---
+++ src/timus/task1000.java
@@ -0,0 +1,16 @@
+package timus;
+
+import java.util.Scanner;
+
+public class task1000 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int x = in.nextInt();
+        int y = in.nextInt();
+        int z = x+y;
+
+        System.out.printf("%d \n", z);
+
+    }
+}
