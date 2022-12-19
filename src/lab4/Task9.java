0600b62366e5c2835e4dbaefbd98bf3da69e063d
DmitrySofronov <sofronov.d.v@yandex.ru>
2022-12-20 01:48:32 +0500

    лабораторная работа 4

--- src/lab4/Task9.java
+++ src/lab4/Task9.java
@@ -1,7 +1,82 @@
 package lab4;
 
-public class Task9 {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Task9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите текст на английском языке для шифрования:");
+        String text = scanner.nextLine();
+        System.out.println("Введите ключ шифрования:");
+        int key = scanner.nextInt();
+        String encryptedText = encrypt(text, key);
+        System.out.println("Текст после преобразования: " + encryptedText);
+        System.out.println("Выполнить обратное преобразование? (y/n)");
+        String answer = scanner.next();
+        if (answer.equals("y")) {
+            String decryptedText = decrypt(encryptedText, key);
+            System.out.println("Текст после обратного преобразования: " + decryptedText);
+        } else if (answer.equals("n")) {
+            System.out.println("До свидания!");
+        } else {
+            System.out.println("Введите корректный ответ:");
+        }
+    }
+
+    public static String encrypt(String text, int key) {
+        StringBuilder encryptedText = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            if (Character.isLetter(ch)) {
+                if (Character.isLowerCase(ch)) {
+                    char c = (char) (ch + key);
+                    if (c > 'z') {
+                        encryptedText.append((char) (ch - (26 - key)));
+                    } else {
+                        encryptedText.append(c);
+                    }
+                } else if (Character.isUpperCase(ch)) {
+                    char c = (char) (ch + key);
+                    if (c > 'Z') {
+                        encryptedText.append((char) (ch - (26 - key)));
+                    } else {
+                        encryptedText.append(c);
+                    }
+                }
+            } else {
+                encryptedText.append(ch);
+            }
+        }
+        return encryptedText.toString();
+    }
+
+    public static String decrypt(String text, int key) {
+        StringBuilder decryptedText = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            if (Character.isLetter(ch)) {
+                if (Character.isLowerCase(ch)) {
+                    char c = (char) (ch - key);
+                    if (c < 'a') {
+                        decryptedText.append((char) (ch + (26 - key)));
+                    } else {
+                        decryptedText.append(c);
+                    }
+                } else if (Character.isUpperCase(ch)) {
+                    char c = (char) (ch - key);
+                    if (c < 'A') {
+                        decryptedText.append((char) (ch + (26 - key)));
+                    } else {
+                        decryptedText.append(c);
+                    }
+                }
+            } else {
+                decryptedText.append(ch);
+            }
+        }
+        return decryptedText.toString();
+    }
 }
 
 /*(дополнительная задача). Напишите программу
